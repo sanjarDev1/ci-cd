b4cd14defd4f5160f85f206ba2a8c7ffafb45830
Sanjarbek <sanjarbek>
2024-06-20 14:58:38 +0500

    code push

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
+        <h1>hello world</h1>
         Click on the Vite and React logos to learn more
       </p>
     </>
